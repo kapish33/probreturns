ef6eaa63bc6f965332352abc944a08450d19a3c2
Shiva <kapishsingh2209@gmail.com>
2022-10-02 09:57:05 +0530

    alt and map

--- components/HomePage/Filters.jsx
+++ components/HomePage/Filters.jsx
@@ -12,23 +12,25 @@ import { filter } from '../../constant/filterConat';
 
 const Filters = () => {
   return (
-    <Flex minWidth='max-content' alignItems='center' gap='2'>
-      <InputGroup>
-        <InputLeftElement
-          pointerEvents='none'
-          children={<Search2Icon color='gray.300' />}
-        />
-        <Input type='tel' placeholder='Enter an address city or Zip Code' />
-      </InputGroup>
-      <Spacer />
-      <ButtonGroup gap='2'>
-        {filter.map((buttonData, i) => (
-          <Button key={i} {...buttonData}>
-            {buttonData.name}
-          </Button>
-        ))}
-      </ButtonGroup>
-    </Flex>
+    <>
+      <Flex minWidth='max-content' alignItems='center' gap='2'>
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents='none'
+            children={<Search2Icon color='gray.300' />}
+          />
+          <Input type='tel' placeholder='Enter an address city or Zip Code' />
+        </InputGroup>
+        <Spacer />
+        <ButtonGroup gap='2'>
+          {filter.map((buttonData, i) => (
+            <Button key={i} {...buttonData}>
+              {buttonData.name}
+            </Button>
+          ))}
+        </ButtonGroup>
+      </Flex>
+    </>
   );
 };
 
